9c9886106bb88049024c8f6249884fa069c6356d
Hristo Hristov <hrshristov@gmail.com>
2024-02-28 22:32:29 +0200

    Initialize project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Post from "./components/Post";
+
+function App() {
+    return (
+        <main>
+            <Post author="Hristo" body="React.js" />
+            <Post />
+        </main>
+    );
+}
+
+export default App;
---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+import classes from "./Post.module.css";
+
+const Post = ({ author, body }) => {
+    return (
+        <div className={classes.post}>
+            <p>{author}</p>
+            <p>{body}</p>
+        </div>
+    );
+};
+
+export default Post;
