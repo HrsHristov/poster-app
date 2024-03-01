c84cfcfa1c9e8d23aeb01cd13e034e0cef347a23
Hristo Hristov <hrshristov@gmail.com>
2024-03-01 13:48:38 +0200

    Add list of posts

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,45 @@
+import { useState } from "react";
+
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
+import Modal from "./Modal";
 
-const PostsList = () => {
+const PostsList = ({ isPosting, onStopPosting }) => {
+    const [posts, setPosts] = useState([]);
+
+    const addPostHandler = (postData) => {
+        setPosts((existingPosts) => [postData, ...existingPosts]);
+    };
+
     return (
-        <ul className={classes.posts}>
-            <Post author="Hristo" body="React.js" />
-            <Post author="Denitsa" body="TypeScript" />
-        </ul>
+        <>
+            {isPosting && (
+                <Modal onClose={onStopPosting}>
+                    <NewPost
+                        onCancel={onStopPosting}
+                        onAddPost={addPostHandler}
+                    />
+                </Modal>
+            )}
+            {posts.length > 0 && (
+                <ul className={classes.posts}>
+                    {posts.map((post) => (
+                        <Post
+                            key={post.body}
+                            author={post.author}
+                            body={post.body}
+                        />
+                    ))}
+                </ul>
+            )}
+            {posts.length === 0 && (
+                <div stlye={{ textAlign: "center", color: "white" }}>
+                    <h2>There are no posts yet.</h2>
+                    <p>Start adding some!</p>
+                </div>
+            )}
+        </>
     );
 };
 
