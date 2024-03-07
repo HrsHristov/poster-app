e9ef8e625afae1a6af0108a0c24fc3ed8a1be78b
Hristo Hristov <hrshristov@gmail.com>
2024-03-07 10:30:20 +0200

    Add Router

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,56 +1,19 @@
-import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
-import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
-import Modal from "./Modal";
 
-const PostsList = ({ isPosting, onStopPosting }) => {
-    const [posts, setPosts] = useState([]);
-    const [isFetching, setIsFetching] = useState("false");
-
-    useEffect(() => {
-        const fetchPost = async () => {
-            setIsFetching(true);
-
-            const response = await fetch("http://localhost:8080/posts");
-            const resData = await response.json();
-
-            setPosts(resData.posts);
-
-            setIsFetching(false);
-        };
-
-        fetchPost();
-    }, []);
-
-    const addPostHandler = async (postData) => {
-        await fetch("http://localhost:8080/posts", {
-            method: "POST",
-            body: JSON.stringify(postData),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-
-        setPosts((existingPosts) => [postData, ...existingPosts]);
-    };
+const PostsList = () => {
+    const posts = useLoaderData();
 
     return (
         <>
-            {isPosting && (
-                <Modal onClose={onStopPosting}>
-                    <NewPost
-                        onCancel={onStopPosting}
-                        onAddPost={addPostHandler}
-                    />
-                </Modal>
-            )}
-            {!isFetching && posts.length > 0 && (
+            {posts.length > 0 && (
                 <ul className={classes.posts}>
                     {posts.map((post) => (
                         <Post
-                            key={post.body}
+                            key={post.id}
+                            id={post.id}
                             author={post.author}
                             body={post.body}
                         />
@@ -58,16 +21,10 @@ const PostsList = ({ isPosting, onStopPosting }) => {
                 </ul>
             )}
 
-            {!isFetching && posts.length === 0 && (
+            {posts.length === 0 && (
                 <div style={{ textAlign: "center", color: "white" }}>
                     <h2>There are no posts yet.</h2>
                     <p>Start adding some!</p>
-                </div>
-            )}
-
-            {isFetching && (
-                <div style={{ textAlign: "center", color: "white" }}>
-                    <p>Loading posts...</p>
                 </div>
             )}
         </>
