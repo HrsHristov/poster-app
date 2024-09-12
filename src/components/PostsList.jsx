09ed0e8bb60fa58b347c8464b0057b1f75afa1b7
Hristo Hristov <hrshristov@gmail.com>
2024-09-12 15:06:21 +0300

    Add NewPost component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
+import NewPost from "./NewPost";
 import Post from "./Post";
 
 import classes from "./PostsList.module.css";
 
 const PostsList = () => {
+    const [enteredBody, setEnteredBody] = useState("");
+    const [enteredAuthor, setenteredAuthor] = useState("");
+
+    const bodyChangeHandler = (event) => {
+        setEnteredBody(event.target.value);
+    };
+
+    const authorChangeHandler = (event) => {
+        setenteredAuthor(event.target.value);
+    };
+
     return (
-        <ul className={classes.posts}>
-            <Post author="Hristo" body="React.js refresher" />
-            <Post author="Icaka" body="Next.js up ahead" />
-        </ul>
+        <>
+            <NewPost
+                onBodyChange={bodyChangeHandler}
+                onAuthorChange={authorChangeHandler}
+            />
+            <ul className={classes.posts}>
+                <Post author={enteredAuthor} body={enteredBody} />
+                <Post author="Icaka" body="Next.js up ahead" />
+            </ul>
+        </>
     );
 };
 
